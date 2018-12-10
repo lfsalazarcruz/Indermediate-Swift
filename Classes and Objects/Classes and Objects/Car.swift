81aba7b8debb65ffe465668b6f68be52013abade
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-12-09 23:01:19 -0800

    Car class with properties.
    Class example 1.

---
+++ Classes and Objects/Classes and Objects/Car.swift
@@ -0,0 +1,26 @@
+//
+//  Car.swift
+//  Classes and Objects
+//
+//  Created by luis salazar on 12/8/18.
+//  Copyright © 2018 F Productions. All rights reserved.
+//
+
+import Foundation
+
+enum CarType {
+    case Sedan
+    case Coupe
+    case Hatchback
+}
+
+class Car {
+    
+    var colour = "Black"
+    var numberOfSeats : Int = 5
+    var typeOfCar : CarType = .Coupe
+    
+    init(customerChosenColour : String) {
+        colour = customerChosenColour
+    }
+}
---
+++ Classes and Objects/Classes and Objects/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  Classes and Objects
+//
+//  Created by luis salazar on 12/8/18.
+//  Copyright © 2018 F Productions. All rights reserved.
+//
+
+import Foundation
+
+let myCar = Car(customerChosenColour: q"Red")
+
+print(myCar.colour, myCar.numberOfSeats, myCar.typeOfCar)
+
+
+
